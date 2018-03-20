45320e6219826bc8d3c157d53da9a5d47f48f70c
robsoncler <robsoncler@hotmail.com>
2018-03-19 21:07:23 -0300

    robsoncler
    O codigo apresentado possui três telas sendo: index, login e contato que linkm uma a outra fazendo uma interação de software .

--- core/views.py
+++ core/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
+from django.contrib import messages
+
+
+def index (request):
+	return render(request,'index.html')
+
+def login (request):
+	if request.method == 'GET':
+		messages.success(request, "Huge success!")
+		return render(request,'login.html')
+	elif request.method == 'POST':
+		if request.POST.get('senha') == 'teste123':
+			print('Usuário {} entrou com sucesso!'.format(request.POST.get('email')))
+			return redirect(index)
+		else:
+			print('Usuário {} digitou a senha errada!'.format(request.POST.get('email')))
+			return render(request, 'login.html')
 
 def contato(request):
 	if request.method == 'GET':
